43195d8ea5366d196cea80d2237f52e1216bee8d
Kelly Loyd <kelly.loyd@icloud.com>
2024-02-15 11:53:36 -0500

    Currency update

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { dispatch, budget, expenses } = useContext(AppContext);
+    const { dispatch, budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         let pendingBudget = event.target.value;
@@ -31,7 +31,7 @@ const Budget = () => {
     
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £ </span>
+<span>Budget: {currency}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,13 +1,23 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Currency = () => {
-    const { currency } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
     const [newCurrency, setNewCurrency] = useState('');
-    
+    const handleCurrencyChange = (event) => {
+        let pendingCurrency = event.target.value;
+        setNewCurrency(pendingCurrency);
+
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: pendingCurrency,
+        });
+
+    }
+
     return (
         <div className='col-sm'>
-            <select className="form-control bg-success" id="inputCurrency" onChange={(event) => setNewCurrency(event.target.value)}>
-            <option defaultValue>Currency ({currency})</option>
+            <select className="form-control bg-success" id="inputCurrency" onChange={(event) => handleCurrencyChange(event.target.value)}>
+            <option defaultValue>Currency ({newCurrency})</option>
                 <option value="dollar" name="dollar">$ Dollar</option>
                 <option value="pound" name="pound">£ Pound</option>
                 <option value="euro" name="euro">€ Euro</option>
